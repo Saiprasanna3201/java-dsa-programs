269b2266204f63547b6f33a8c1307d1326615ed7
Saiprasanna3201 <151619962+Saiprasanna3201@users.noreply.github.com>
2025-07-29 19:00:15 +0530

    basic programs.java

---
+++ basics/basic programs.java
@@ -0,0 +1,20 @@
+//write a program that takes an input of age and
+//print if you are an adult or not
+// >=18
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int age =10;
+        
+        if(age >= 18) {
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are not an adult");
+        }
+        scanner.close();
+    }
+}
